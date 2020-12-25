ec461d76891000ffff8003be570ebe26d787f4aa
C. L <amautacelt@gmail.com>
2020-12-25 05:56:09 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -20,3 +20,14 @@ def loop_message_n_times(message, number)
 end
 
 loop_message_n_times("Hello Red Balloon.\n", 10)
+
+
+def output_array(array)
+  count = 0
+  while count < array.length do
+    puts array[count]
+    count += 1
+  end
+end
+
+output_array()
