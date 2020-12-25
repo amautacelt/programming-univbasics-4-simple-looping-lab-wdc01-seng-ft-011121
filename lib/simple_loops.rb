6bebb12f8c8a35a1170a22631f50ecca418330ef
C. L <amautacelt@gmail.com>
2020-12-25 05:51:49 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -1,1 +1,22 @@
 # Write your methods here
+
+def loop_message_five_times(message)
+  count = 0
+  while count < 5 do
+    puts message
+    count += 1
+  end
+end
+
+#loop_message_five_times("Hello World.\n")
+
+
+def loop_message_n_times(message, number)
+  count = 0
+  while count < number do
+    puts message
+    count =+ 1
+  end
+end
+
+loop_message_n_times("Hello Red Balloon.\n", 10)
